d94c346f00af80a43df26b9a38490c1b9ca1b1ac
Dahir Muse Abdulle <138979678+Duceysaneyare@users.noreply.github.com>
2025-04-18 21:41:15 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<div id="app">
+  <calculator-app></calculator-app>
+
+</div>
+</template>
+
+<script>
+import CalculatorApp from './components/CalculatorApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CalculatorApp
+  }
+}
+</script>
+
+<style>
+body{
+  background: #31475e !important;
+}
+</style>
